d7bd2d85ad0013e4b10733591233282af480a8d5
Santiago Magariños <san.magarinos@gmail.com>
2020-01-13 20:00:55 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# GIF generator
+
+Generate GIF from single image.
+
+positional arguments:
+  file_name     File name
+  w_images      How many times is the image in the horizontal axis of the
+                file?
+  total_images  How many images are there in the file?
+
+optional arguments:
+  -h, --help    show this help message and exit
+
+## Installation
+
+pip install -r requirements.txt
+
+## Example
+
+python gif_gen.py yoga_frog.png 8 48
---
+++ gif_gen.py
@@ -0,0 +1,28 @@
+import math
+import argparse
+from PIL import Image
+
+parser = argparse.ArgumentParser(description='Generate GIF from single image.')
+parser.add_argument('file_name', type=str, help='File name')
+parser.add_argument('w_images', type=int, help='How many times is the image in the horizontal axis of the file?')
+parser.add_argument('total_images', type=int, help='How many images are there in the file?')
+args = parser.parse_args()
+
+with Image.open(args.file_name) as im:
+    w, h = im.size
+    h_images = math.ceil(args.total_images / args.w_images)
+    assert(w % args.w_images == 0 and h % h_images == 0) # Exact divisions
+    sprite_w = w / args.w_images
+    sprite_h = h / h_images
+    gif_images = []
+    for i in range(args.total_images):
+        pos_x = i % args.w_images * sprite_w     # (0...7)(0...7)(0...7) * W
+        pos_y = i // args.w_images * sprite_h    # (0...0)(1...1)(2...2) * H
+        gif_images.append(im.crop((pos_x, pos_y, pos_x + sprite_w, pos_y + sprite_h)))
+    gif_images[0].save('yoga_frog.gif',
+                       save_all=True,
+                       append_images=gif_images[1:],
+                       optimize=False, 
+                       duration=40, 
+                       loop=0
+                      )
