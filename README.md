c0fe84380bed84edc06fdef196ec8a7403da215b
Santiago Magariños <san.magarinos@gmail.com>
2020-01-17 13:42:46 -0300

    Generate from directory

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # GIF generator
 
-Generate GIF from single image.
+Generate GIF from a sprite sheet or directory.
+
+usage:
+
+~~~~
+gif_gen.py [-h] [--w W] [--t T] [--d D] [--loop LOOP] src
+~~~~
 
 positional arguments:
-  * file_name: File name
-  * w_images: How many times is the image in the horizontal axis of the file?
-  * total_images: How many images are there in the file?
+  * src: File or directory name
 
 optional arguments:
-  * -h, --help    show this help message and exit
+  * -h, --help: show this help message and exit
+  * --w W: Width of the sprite sheet in number of images. Required for sprite sheets.
+  * --t T: Total number of images on the sprite sheet. Required for sprite sheets.
+  * --d D: The time to display the current frame of the GIF, in milliseconds.
+  * --loop LOOP: The number of times the GIF should loop. Do not set or set to 0 for infinite.
 
 ## Installation
 
@@ -19,12 +27,16 @@ pip install -r requirements.txt
 ## Examples
 
 ~~~~
-$ python gif_gen.py yoga_frog.png 8 48
+$ python gif_gen.py examples/yoga_frog.png --w 8 --t 48
 ~~~~
 
 ~~~~
-$ python gif_gen.py fox.png 6 28
+$ python gif_gen.py examples/fox.png --w 6 --t 28 --d 50
+~~~~
+
+~~~~
+$ python gif_gen.py examples/shrine --d 500
 ~~~~
 
 ## Next
-* Folder mode
+* Multi-animation
--- gif_gen.py
+++ gif_gen.py
@@ -3,27 +3,49 @@ import argparse
 import os
 from PIL import Image
 
-parser = argparse.ArgumentParser(description='Generate GIF from single image.')
-parser.add_argument('file_name', type=str, help='File name')
-parser.add_argument('w_images', type=int, help='How many times is the image in the horizontal axis of the file?')
-parser.add_argument('total_images', type=int, help='How many images are there in the file?')
+
+parser = argparse.ArgumentParser(description='GIF Generator. '
+                                 'Generate GIF from a sprite sheet or directory.')
+parser.add_argument('src', type=str, help='File or directory name')
+parser.add_argument('--w',
+                    type=int, 
+                    help='Width of the sprite sheet in number of images. Required for sprite sheets.')
+parser.add_argument('--t',
+                    type=int, 
+                    help='Total number of images on the sprite sheet. Required for sprite sheets.')
+parser.add_argument('--d',
+                    type=int, 
+                    help='The time to display the current frame of the GIF, in milliseconds.')
+parser.add_argument('--loop',
+                    type=int, 
+                    help='The number of times the GIF should loop. Do not set or set to 0 for infinite.')                                        
 args = parser.parse_args()
 
-with Image.open(args.file_name) as im:
-    w, h = im.size
-    h_images = math.ceil(args.total_images / args.w_images)
-    assert(w % args.w_images == 0 and h % h_images == 0) # Exact divisions
-    sprite_w = w / args.w_images
-    sprite_h = h / h_images
-    gif_images = []
-    for i in range(args.total_images):
-        pos_x = i % args.w_images * sprite_w     # (0...7)(0...7)(0...7) * W
-        pos_y = i // args.w_images * sprite_h    # (0...0)(1...1)(2...2) * H
-        gif_images.append(im.crop((pos_x, pos_y, pos_x + sprite_w, pos_y + sprite_h)))
-        fn, _ = os.path.splitext(args.file_name)
-    gif_images[0].save('{}.gif'.format(fn),
-                       save_all=True,
-                       append_images=gif_images[1:],
-                       optimize=False, 
-                       duration=40, 
-                       loop=0)
+
+gif_images = []
+if os.path.isfile(args.src):
+    if args.w is None or args.t is None:
+        raise ValueError('--w and --t parameters are required for sprite sheets')
+    with Image.open(args.src) as im:
+        w, h = im.size
+        h_images = math.ceil(args.t / args.w)
+        assert(w % args.w == 0 and h % h_images == 0) # Exact divisions
+        sprite_w = w / args.w
+        sprite_h = h / h_images
+        for i in range(args.t):
+            pos_x = i % args.w * sprite_w     # (0...7)(0...7)(0...7) * W
+            pos_y = i // args.w * sprite_h    # (0...0)(1...1)(2...2) * H
+            gif_images.append(im.crop((pos_x, pos_y, 
+                                       pos_x + sprite_w, pos_y + sprite_h)))
+        fn, _ = os.path.splitext(args.src)
+else:
+    gif_images = [Image.open(os.path.join(args.src, f)) 
+                  for f in os.listdir(args.src)]
+    fn = args.src
+
+gif_images[0].save('{}.gif'.format(fn),
+                   save_all=True,
+                   append_images=gif_images[1:],
+                   optimize=False,
+                   duration=args.d or 200, 
+                   loop=args.loop or 0)
